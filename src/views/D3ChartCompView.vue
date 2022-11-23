c9605894ed6beda70630b067c2c28b8f105fef3b
Luciano Nocera <nocera@usc.edu>
2022-11-22 21:21:24 -0800

    - events within chart - events between components and chart

--- src/views/D3ChartCompView.vue
+++ src/views/D3ChartCompView.vue
@@ -3,12 +3,16 @@
 
     <h1 class="mt-3">D3 Chart Composition API</h1>
 
+    <button type="button" class="btn btn-danger me-2" @click="clear">Clear</button>
+    <button type="button" class="btn btn-primary me-2" @click="reset">Reset</button>
+    <button type="button" class="btn btn-secondary" @click="invert">Invert</button>
+
     <div class="title">Top 10 COVID-19 confirmed in US, Aug 31 2020 (source Johns Hopkins University)</div>
     <svg width="400" height="300" id="chart"></svg>
 
     <h2>Data</h2>
     {{message}}
-    <pre><code>{{store.planets}}</code></pre>
+    <pre><code>{{data}}</code></pre>
 
   </div>
 </template>
@@ -20,41 +24,80 @@ import { useChartStore } from '../stores/useChartStore'
 
 const store = useChartStore();
 const message = ref('Data was set from store');
+const data = ref([]);
 
-function barChart() {
+function clear() {
+  const svg = d3.select('#chart');
+  svg.selectAll('rect')
+    .remove();
+  svg.selectAll('text')
+    .remove();
+}
+
+function reset() {
+  data.value = JSON.parse(JSON.stringify(store.planets));
+  clear();
+  barChart(data.value);
+}
+
+function invert() {
+  data.value.reverse();
+  clear();
+  barChart(data.value);
+}
+
+function updateText(d) {
+  d3.select('#chart')
+    .select('#text')
+    .text(d.label);
+}
+
+function barChart(data) {
+  console.log('barChart')
   var svg = d3.select('#chart');
   var enter = svg.selectAll('rect')
-    .data(store.planets)
+    .data(data)
     .enter();
 
   enter.append('rect')
     .attr('x', 100)
     .attr('y', (d, i) => 20 + i * 25)
     .attr('width', d => d.dist / 25)
-    .attr('class', 'bar');
+    .attr('class', 'bar')
+    .on('click', (event, d) => updateText(d));
 
   enter.append('text')
     .attr('x', 90)
     .attr('y', (d, i) => 30 + i * 25)
     .attr('class', 'label')
-    .text(d => d.label);
+    .text(d => d.label)
+    .on('click', (event, d) => updateText(d));
 
   enter.append('text')
     .attr('x', d => 150 + d.dist / 25)
     .attr('y', (d, i) => 30 + i * 25)
     .attr('class', 'value')
-    .text(d => Number(d.dist).toLocaleString());
+    .text(d => Number(d.dist).toLocaleString())
+    .on('click', (event, d) => updateText(d));
+
+  enter.append('text')
+    .attr('x', 300)
+    .attr('y', 100)
+    .attr('id', 'text')
+    .attr('class', 'value');
+
 }
 
 onMounted(() => {
   if (!store.planets) {
     d3.json('planets.json').then(result => {
       store.planets = result;
+      data.value = JSON.parse(JSON.stringify(store.planets));
       message.value = `Data was loaded from file, contains ${store.planets.length} rows`;
-      barChart();
+      barChart(store.planets);
     });
   } else {
-    barChart();
+    barChart(store.planets);
   }
 });
 </script>
@@ -73,7 +116,7 @@ pre {
   text-anchor: end;
   alignment-baseline: middle;
   font-size: 12px;
-  fill: black;
+  fill: #eee;
 }
 
 :deep(text.value) {
